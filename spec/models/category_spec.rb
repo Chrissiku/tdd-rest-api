99e4f3601196dd786bad7542a5dc77bf312f0b43
Chris Siku <chrissiku5@gmail.com>
2022-11-30 14:05:28 +0200

    Write the model specs for Category and Book

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # Association test
+  it { should have_many(:books) }
+  # Validation tests
+  it { should validate_presence_of(:name) }
+  it {
+    should validate_length_of(:name)
+      .is_at_least(3)
+ }
 end
