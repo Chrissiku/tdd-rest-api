33dec5186e828d03a7fef38819cdc7976be13ef4
Chris Siku <chrissiku5@gmail.com>
2022-12-01 11:05:14 +0200

    Setup book's validations and association

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,5 @@
 class Book < ApplicationRecord
   belongs_to :category
+  belongs_to :user
   validates :title, :author, presence: true, length: { minimum: 3 }
 end
--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe Book, type: :model do
   # Association test
   it { should belong_to(:category) }
+  it { should belong_to(:user) }
    # Validation tests
   it { should validate_presence_of(:title) }
   it { should validate_presence_of(:author) }
